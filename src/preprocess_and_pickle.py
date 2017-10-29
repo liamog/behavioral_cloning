774e12f876b974a85425cae24f424d97ddc6a5fc
liamog <liam@liamogorman.com>
2017-10-29 09:19:27 -0700

    use preprocess module

--- src/preprocess_and_pickle.py
+++ src/preprocess_and_pickle.py
@@ -1,29 +1,27 @@
 import csv
 import cv2
 import numpy as np
-
-crop_x1 = 0
-crop_x2 = 320
-crop_y1 = 60
-crop_y2 = 140
+import preprocess as prep
 
 lines = []
 with open('../data/raw/driving_log.csv') as csvfile:
-  reader = csv.reader(csvfile)
-  for line in reader:
-    lines.append(line)
+    reader = csv.reader(csvfile)
+    for line in reader:
+        if not line:
+            continue
+        lines.append(line)
 
 images = []
 measurements = []
 for line in lines:
-  source_path = line[0]
-  filename = source_path.split('/')[-1]
-  current_path = '../data/raw/IMG/' + filename
-  image = cv2.imread(current_path)
-  crop_img = image[crop_y1:crop_y2, crop_x1:crop_x2]
-  images.append(crop_img)
-  measurement = float(line[3])
-  measurements.append(measurement)
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = '../data/raw/IMG/' + filename
+    image = cv2.imread(current_path)
+    crop_img = prep.preprocess_image(image)
+    images.append(crop_img)
+    measurement = float(line[3])
+    measurements.append(measurement)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
