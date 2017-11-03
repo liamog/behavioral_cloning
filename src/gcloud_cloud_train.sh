db7d32c7742b4b253e9c0e26ad4cdfb56930a027
liamog <liam@liamogorman.com>
2017-11-03 00:36:03 -0700

    Training and validation sets to use in training

--- src/gcloud_cloud_train.sh
+++ src/gcloud_cloud_train.sh
@@ -7,10 +7,12 @@ GS_DIR="gs://liamog_udacity/bh_cloning"
 
 JOB_DIR=$GS_DIR"_$now/output"
 
-TRAIN_FILE=$GS_DIR"/train.npz"
-EPOCHS=2
+TRAINING_FILES="$GS_DIR/right_turn_train.npz $GS_DIR/swerving_trimmed_train.npz $GS_DIR/lap_with_mouse_train.npz $GS_DIR/track1_train.npz"
+VALIDATION_FILES="$GS_DIR/right_turn_valid.npz $GS_DIR/swerving_trimmed_valid.npz $GS_DIR/lap_with_mouse_valid.npz $GS_DIR/track1_valid.npz"
 
-echo $X_TRAIN_FILE
+echo $TRAINING_FILES
+echo $VALIDATION_FILES
+EPOCHS=5
 
 gcloud ml-engine jobs submit training $JOB_NAME \
                                     --stream-logs \
@@ -20,7 +22,8 @@ gcloud ml-engine jobs submit training $JOB_NAME \
                                     --module-name trainer.task \
                                     --region us-central1 \
                                     -- \
-                                    --train_file $TRAIN_FILE \
+                                    --training_files $TRAINING_FILES \
+                                    --validation_files $VALIDATION_FILES \
                                     --job-dir $JOB_DIR \
                                     --num-epochs $EPOCHS
 popd
--- src/gcloud_local_train.sh
+++ src/gcloud_local_train.sh
@@ -2,13 +2,13 @@
 pushd .
 cd "$(dirname "$0")"
 
-TRAINING_FILES='../data/proccessed_and_pickled/swerving_full_train.npz ../data/proccessed_and_pickled/lap_with_mouse_train.npz ../data/proccessed_and_pickled/track1_train.npz'
-VALIDAION_FILES='../data/proccessed_and_pickled/swerving_full_valid.npz ../data/proccessed_and_pickled/lap_with_mouse_valid.npz ../data/proccessed_and_pickled/track1_valid.npz'
+TRAINING_FILES='../data/proccessed_and_pickled/right_turn_train.npz ../data/proccessed_and_pickled/swerving_trimmed_train.npz ../data/proccessed_and_pickled/lap_with_mouse_train.npz ../data/proccessed_and_pickled/track1_train.npz'
+VALIDAION_FILES='../data/proccessed_and_pickled/right_turn_valid.npz ../data/proccessed_and_pickled/swerving_trimmed_valid.npz ../data/proccessed_and_pickled/lap_with_mouse_valid.npz ../data/proccessed_and_pickled/track1_valid.npz'
 JOB_DIR=../output/
 
 python 'cloud-ml/trainer/task.py' --training_files $TRAINING_FILES \
                        --validation_files $VALIDAION_FILES \
                        --job-dir $JOB_DIR \
-                       --num-epochs 5 \
+                       --num-epochs 1 \
                        --h5py
 popd
