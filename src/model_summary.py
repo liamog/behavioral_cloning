8eac22b7093123733a30fb9b238ee893fd89b7b7
Liam O'Gorman <liam@liamogorman.com>
2017-11-07 09:02:43 -0800

    model summary script

--- src/model_summary.py
+++ src/model_summary.py
@@ -1,5 +1,8 @@
 from keras.models import load_model
+from keras.utils import plot_model
 
 
 model = load_model('/Users/liamog/udacity/behavioural_cloning/src/output/weights-improvement-00-0.17.hdf5')
 print(model.summary())
+
+plot_model(model, to_file='model.png')
