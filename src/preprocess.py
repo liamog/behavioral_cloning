e2a0bc3d5645f02431a5c1ed07f8c7530b03fcd6
liamog <liam@liamogorman.com>
2017-11-07 00:38:59 -0800

    preprocess to YUV

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,10 @@
+import cv2
+
 
 def preprocess_image(image):
     crop_x1 = 0
     crop_x2 = 320
     crop_y1 = 60
     crop_y2 = 140
-    return image[crop_y1:crop_y2,
-                 crop_x1:crop_x2]
+    return cv2.cvtColor(image[crop_y1:crop_y2,
+                              crop_x1:crop_x2], cv2.COLOR_BGR2YUV)
