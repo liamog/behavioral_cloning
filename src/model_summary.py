af408098024f56b86a9546ca2e466501007458a0
Liam O'Gorman <liam@liamogorman.com>
2017-11-07 20:43:22 -0800

    Model summary and png visualization from Keras

--- src/model_summary.py
+++ src/model_summary.py
@@ -1,8 +1,8 @@
 from keras.models import load_model
-from keras.utils import plot_model
+from keras.utils.vis_utils import plot_model
 
 
-model = load_model('/Users/liamog/udacity/behavioural_cloning/src/output/weights-improvement-00-0.17.hdf5')
+model = load_model('/home/liam/udacity/behavioral_cloning/src/output/bh_clone.hdf5')
 print(model.summary())
 
-plot_model(model, to_file='model.png')
+plot_model(model, to_file='model.png', show_shapes=True)
