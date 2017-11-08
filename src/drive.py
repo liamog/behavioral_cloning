4d402364d35552b8938d221a10cb3a3e7b339479
liamog <liam@liamogorman.com>
2017-11-08 08:55:01 -0800

    Convert BGR to RGB prior to cropping and YUV conversion

--- src/drive.py
+++ src/drive.py
@@ -61,8 +61,9 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = prep.preprocess_image(np.asarray(image))
-
+        input_array = np.asarray(image)
+        image_array = input_array[:, :, ::-1]
+        image_array = prep.preprocess_image(image_array)
         steering_angle = float(model.predict(
             image_array[None, :, :, :], batch_size=1))
 
